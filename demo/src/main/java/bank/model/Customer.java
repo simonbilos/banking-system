3f62c8659f99c2e77970017b27289c56bf8fbaf9
Šimon Biloš <bilos.simon@gmail.com>
2026-03-26 09:57:08 +0100

    added a method to validate the phone number

--- demo/src/main/java/bank/model/Customer.java
+++ demo/src/main/java/bank/model/Customer.java
@@ -1,10 +1,34 @@
 package bank.model;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Customer {
+    private String customerId;
     private String firstName;
     private String surname;
     private String dateOfBirth;
     private String phoneNumber;
+
+    public void updatePhoneNumber(String newPhoneNumber) {
+        if (checkPhoneNumber(newPhoneNumber)) {
+            this.phoneNumber = newPhoneNumber;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid phone number format");
+        }
+    }
+
+    public boolean checkPhoneNumber(String phoneNumber) {
+        String pattern = "(^09\\d{2}\\s?\\d{3}\\s?\\d{3}|^\\+421\\s?\\d{3}\\s?\\d{3}\\s?\\d{3})$";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(phoneNumber);
+        return m.matches();
+    }
+
+    public String getCustomerId() {
+        return this.customerId;
+    }
 
     public String getFirstName() {
         return this.firstName;
