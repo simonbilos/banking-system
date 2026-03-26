fb8b5334294a7aba4fb1c994c0a2e7f74144e7a1
Šimon Biloš <bilos.simon@gmail.com>
2026-03-26 08:58:24 +0100

    Customer getters

---
+++ demo/src/main/java/bank/model/Customer.java
@@ -0,0 +1,24 @@
+package bank.model;
+
+public class Customer {
+    private String firstName;
+    private String surname;
+    private String dateOfBirth;
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public String getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+}
